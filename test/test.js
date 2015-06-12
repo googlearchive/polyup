b75cd03cbdf63cac462bbd5f5c90b3dc5d9d5417
Peter Burns <rictic@google.com>
2015-06-12 16:04:17 -0700

    minor updates

--- test/test.js
+++ test/test.js
@@ -1,6 +1,6 @@
 var assert = require('assert');
 var path = require('path');
-var upgradeHtml = require('../lib/main');
+var upgradeHtml = require('../lib/upgrade_html');
 var fs = require('fs');
 
 
@@ -10,17 +10,13 @@ suite('Upgrading HTML', function() {
   var files = fs.readdirSync(fixturesDir);
   files.forEach(function(filename) {
     if (!/\.html$/.test(filename)) {
-      return; // skip
-    }
-    if (/simple-element/.test(filename)) {
-      // return; // skip, as we aren't finished upgrading this element
+      return; // We only want the html files.
     }
     test('upgrade ' + filename, function() {
       var fullPath = path.join(fixturesDir, filename);
       var actual = upgradeHtml(fullPath);
       var expected = fs.readFileSync(fullPath + '.out', 'utf-8');
       assert.equal(expected, actual);
-      // expect('<root>' + expected + '</root>').xml.to.deep.equal('<root>' + actual + '</root>');
     });
   });
 });
